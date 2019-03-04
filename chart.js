7d9aa735322f4f07f41db4cbfb23d3f674a33904
Mahyuna <xzye123@naver.com>
2019-03-04 11:29:06 +0900

    html문서에 data값 받아서 적용시키기

--- chart.js
+++ chart.js
@@ -1,96 +1,101 @@
-// data
-var data = [
-    { 'year': '2015', 'data': 1, 'color':'pink' },
-    { 'year': '2016', 'data': 4, 'color':'skyblue' },
-    { 'year': '2017', 'data': 15, 'color':'green' },
-    { 'year': '2018', 'data': 20, 'color':'orange' },
-    { 'year': '2019', 'data': 130, 'color':'red' },
-]
+function barChart (data) {
+    // data
 
-// svg size
-var chartWidth = document.getElementById('chart').getBoundingClientRect().width;
-var chartHeight = document.getElementById('chart').getBoundingClientRect().height;
+    // var data = [
+    //     { 'year': '2015', 'data': 1, 'color':'pink' },
+    //     { 'year': '2016', 'data': 4, 'color':'skyblue' },
+    //     { 'year': '2017', 'data': 15, 'color':'green' },
+    //     { 'year': '2018', 'data': 20, 'color':'orange' },
+    //     { 'year': '2019', 'data': 130, 'color':'red' },
+    // ]
 
-var barMaxHeight = 200;
+    // console.log(data)
 
-// create SVG
-var svg = d3.select("#chart")
-    .append("svg")
-        .attr('class','svg')
-        .attr('width', chartWidth)
-        .attr('height', chartHeight)
-;
+    // svg size
+    var chartWidth = document.getElementById('chart').getBoundingClientRect().width;
+    var chartHeight = document.getElementById('chart').getBoundingClientRect().height;
 
-// bar Chart Drawing
-barGap = (160/data.length);
-var barDrawWidth = chartWidth - barGap;
-var barDrawHeight = chartHeight - (barGap*4);
+    var barMaxHeight = 200;
 
-var barDataAlign = ((barDrawWidth / data.length)-barGap) / 2;
+    // create SVG
+    var svg = d3.select("#chart")
+        .append("svg")
+            .attr('class','svg')
+            .attr('width', chartWidth)
+            .attr('height', chartHeight)
+    ;
 
-var y = d3.scaleLinear()
-    .range([barMaxHeight, 0])
-    .domain(
-        [0, d3.max(data, function(d) { return d.data; })]
-    )
-;
+    // bar Chart Drawing
+    barGap = (160/data.length);
+    var barDrawWidth = chartWidth - barGap;
+    var barDrawHeight = chartHeight - (barGap*4);
+
+    var barDataAlign = ((barDrawWidth / data.length)-barGap) / 2;
+
+    var y = d3.scaleLinear()
+        .range([barMaxHeight, 0])
+        .domain(
+            [0, d3.max(data, function(d) { return d.data; })]
+        )
+    ;
 
 
-svg.append('g').attr('class','foreground')
-    .selectAll("rect")
+    svg.append('g').attr('class','foreground')
+        .selectAll("rect")
+            .data(data)
+            .enter().append("rect")
+                .attr("class", "bar")
+                .attr('width', function(d, i){
+                    return (barDrawWidth / data.length)-barGap
+                })
+                .attr("height", function(d, i) {
+                    console.log( "data " + i + " : " + d.data*data.length )
+                        return barMaxHeight - y(d.data)
+                    })
+                .attr("x", function(d, i) {
+                        return (((barDrawWidth / data.length)) * i)
+                    })
+                .attr("y", function(d, i) {
+                        return 55 + y(d.data)
+                    })
+                .style('fill',function(d) {
+                        return d.color
+                    })
+    ;
+
+    svg.append('g')
+        .selectAll('text')
         .data(data)
-        .enter().append("rect")
-            .attr("class", "bar")
-            .attr('width', function(d, i){
-                return (barDrawWidth / data.length)-barGap
-            })
-            .attr("height", function(d, i) {
-                console.log( "data " + i + " : " + d.data*data.length )
-                    return barMaxHeight - y(d.data)
-                })
+        .enter()
+        .append('text')
+            .attr("class", "bar-data")
             .attr("x", function(d, i) {
-                    return (((barDrawWidth / data.length)) * i)
-                })
+                return ((barDrawWidth / data.length)) * i + barDataAlign
+            })
             .attr("y", function(d, i) {
-                    return 55 + y(d.data)
-                })
-            .style('fill',function(d) {
-                    return d.color
-                })
-;
-
-svg.append('g')
-    .selectAll('text')
-    .data(data)
-    .enter()
-    .append('text')
-        .attr("class", "bar-data")
-        .attr("x", function(d, i) {
-            return ((barDrawWidth / data.length)) * i + barDataAlign
-        })
-        .attr("y", function(d, i) {
-            return 55 + y(d.data)
-        })
-        .text(function(d){return d.data})
-        .style('text-anchor','middle')
-;
+                return 55 + y(d.data)
+            })
+            .text(function(d){return d.data})
+            .style('text-anchor','middle')
+    ;
 
 
-svg.append('g')
-    .selectAll('text')
-    .data(data)
-    .enter()
-    .append('text')
-        .attr("class", "bar-year")
-        .attr("x", function(d, i) {
-            return ((barDrawWidth / data.length)) * i + barDataAlign
-        })
-        .attr("y", function(d, i) {
-            return 270
-        })
-        .text(function(d){return d.year})
-        .style('text-anchor','middle')
-;
+    svg.append('g')
+        .selectAll('text')
+        .data(data)
+        .enter()
+        .append('text')
+            .attr("class", "bar-year")
+            .attr("x", function(d, i) {
+                return ((barDrawWidth / data.length)) * i + barDataAlign
+            })
+            .attr("y", function(d, i) {
+                return 270
+            })
+            .text(function(d){return d.year})
+            .style('text-anchor','middle')
+    ;
+}
 
 
 
